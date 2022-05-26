a099b91826a9174369005d2b7084079189efc5a3
Tyler <tygary@gmail.com>
2022-05-25 22:32:06 -0700

    Modifying pixels

--- lighting/PixelControl.py
+++ lighting/PixelControl.py
@@ -11,11 +11,11 @@ class PixelControl(object):
     LED_BRIGHTNESS = 200   # Set to 0 for darkest and 255 for brightest
     LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
     LED_CHANNEL = 0
-    LED_STRIP = ws.SK6812_STRIP_RGBW
+#     LED_STRIP = ws.SK6812_STRIP_RGBW
 
     def __init__(self, ledCount):
         self.LED_COUNT = ledCount
-        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL, self.LED_STRIP)
+        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL)
         self.strip.begin()
 
     def setColor(self, channel, color):

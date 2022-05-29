0a518ce90fbe0c3b58161275e5e9cf74dbfcd340
Tyler <tygary@gmail.com>
2022-05-28 22:32:25 -0700

    Fixing buttons

--- lighting/GenericButtonController.py
+++ lighting/GenericButtonController.py
@@ -9,20 +9,20 @@ class GenericButtonController(object):
 
     def setup_pin(self, pin):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.remove_event_detect(pin)
 
     def add_event_detection(self, pin, callback, bothdirections=False):
         self.setup_pin(pin)
         try:
-            GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+            GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
             if bothdirections:
-                GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+                GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
         except RuntimeError:
             try:
                 GPIO.remove_event_detect(pin)
-                GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+                GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
                 if bothdirections:
-                    GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+                    GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
             except RuntimeError:
                 pass

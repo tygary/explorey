685bcf6a7f0852645e781665e43ad2d0548a2a6e
Tyler Gary <tygary@gmail.com>
2023-05-30 22:38:10 -0700

    .

--- printer/LeverInputController.py
+++ printer/LeverInputController.py
@@ -15,10 +15,10 @@ class LeverInputController(object):
             self.setup_pin(pin)
         self.callback = callback
         # self.thread = threading.Thread(target=self.thread_fn, args=(1,))
-        self.add_event_detection(self.pins[0], self.callback, bothdirections=True)
-        self.add_event_detection(self.pins[1], self.callback, bothdirections=True)
-        self.add_event_detection(self.pins[2], self.callback, bothdirections=True)
-        self.add_event_detection(self.pins[3], self.callback, bothdirections=True)
+        self.add_event_detection(self.pins[0], self.callback, True)
+        self.add_event_detection(self.pins[1], self.callback, True)
+        self.add_event_detection(self.pins[2], self.callback, True)
+        self.add_event_detection(self.pins[3], self.callback, True)
 
     def setup_pin(self, pin):
         GPIO.setmode(GPIO.BOARD)

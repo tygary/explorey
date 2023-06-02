563cb8eb3fdf63f9dad6468db7d4cfe7d138a75e
Tyler Gary <tygary@gmail.com>
2023-06-01 20:00:05 -0700

    .

--- lighting/DmxControl.py
+++ lighting/DmxControl.py
@@ -9,20 +9,20 @@ class DmxControl(object):
         self.dmx = DmxPy.DmxPy("/dev/ttyUSB0")
 
     def setLight(self, channelStart, color):
-        self.dmx.setChannel(channelStart, color[0])
-        self.dmx.setChannel(channelStart + 1, color[1])
-        self.dmx.setChannel(channelStart + 2, color[2])
+        self.dmx.set_channel(channelStart, color[0])
+        self.dmx.set_channel(channelStart + 1, color[1])
+        self.dmx.set_channel(channelStart + 2, color[2])
         # print("set light {} to color {}".format(channelStart, color))
 
     def setParCan(self, channelStart, color):
-        self.dmx.setChannel(channelStart, color[0])
-        self.dmx.setChannel(channelStart + 1, color[1])
-        self.dmx.setChannel(channelStart + 2, color[2])
-        self.dmx.setChannel(channelStart + 3, 255)
-        self.dmx.setChannel(channelStart + 4, 255)
+        self.dmx.set_channel(channelStart, color[0])
+        self.dmx.set_channel(channelStart + 1, color[1])
+        self.dmx.set_channel(channelStart + 2, color[2])
+        self.dmx.set_channel(channelStart + 3, 255)
+        self.dmx.set_channel(channelStart + 4, 255)
 
     def setDimmer(self, channel, brightness):
-        self.dmx.setChannel(channel, brightness)
+        self.dmx.set_channel(channel, brightness)
 
     def render(self):
         self.dmx.render()
@@ -40,11 +40,11 @@ class DmxControl(object):
         # 5  -  Red
         # 6  -  Green
         # 7  -  Blue
-        self.dmx.setChannel(channelStart, 0)
-        self.dmx.setChannel(channelStart + 1, 0)
-        self.dmx.setChannel(channelStart + 2, 0)
-        self.dmx.setChannel(channelStart + 3, brightness)
-        self.dmx.setChannel(channelStart + 4, 255)
-        self.dmx.setChannel(channelStart + 5, 255)
-        self.dmx.setChannel(channelStart + 6, 255)
+        self.dmx.set_channel(channelStart, 0)
+        self.dmx.set_channel(channelStart + 1, 0)
+        self.dmx.set_channel(channelStart + 2, 0)
+        self.dmx.set_channel(channelStart + 3, brightness)
+        self.dmx.set_channel(channelStart + 4, 255)
+        self.dmx.set_channel(channelStart + 5, 255)
+        self.dmx.set_channel(channelStart + 6, 255)
         # print("set blacklight {} to brightness {}".format(channelStart, brightness))

07eff857e0acfce2c579d17f11953b3862d93b4d
Tyler Gary <tygary@gmail.com>
2023-09-28 16:53:18 -0700

    Switching to dates

--- timemachine/TimeMachine.py
+++ timemachine/TimeMachine.py
@@ -1,18 +1,25 @@
 from timemachine.Levers import *
+from datetime import datetime
 import time
 import math
 
-MAX_YEAR = 2999
-MIN_YEAR = 1900
+
+ZERO = datetime.datetime.fromtimestamp(0)
+END = datetime(2999, 12, 31, 23, 59, 59)
+START = datetime(1000, 1, 1, 0, 0, 0)
 # 10 years every second at full speed
-SPEED_MULTIPLIER = -100/1000
+SPEED_MULTIPLIER = -100/1000 * 60 * 60 * 24 * 365
 ZERO_TOLERANCE = 0.1
 MIN_UPDATE_TIME = 0.250
 
 
+def print_datetime(date):
+    return date.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+
 class TimeMachine(object):
     levers = None
-    year = MAX_YEAR
+    date = END
     speed = 0
     active = True
     last_event = time.time()
@@ -37,8 +44,6 @@ class TimeMachine(object):
             log_value = log_value * -1
         return log_value
 
-
-
     def update(self):
         self.levers.update()
         if self.active:
@@ -46,13 +51,15 @@ class TimeMachine(object):
             time_delta = now - self.last_event
             if time_delta > MIN_UPDATE_TIME:
                 change = self.speed * SPEED_MULTIPLIER * time_delta
-                new_year = round(self.year + change)
-                if new_year > MAX_YEAR:
-                    new_year = MAX_YEAR
-                if new_year < MIN_YEAR:
-                    new_year = MIN_YEAR
-                if new_year != self.year:
-                    print(f"Year changed to {new_year}")
-                    self.year = new_year
+                date_ts = datetime.timestamp(self.date)
+                new_date_ts = round(date_ts + change)
+                new_date = datetime.fromtimestamp(new_date_ts)
+                if new_date > END:
+                    new_date = END
+                if new_date < START:
+                    new_date = START
+                if new_date != self.year:
+                    print(f"Year changed to {print_datetime(new_date)}")
+                    self.date = new_date
 
 

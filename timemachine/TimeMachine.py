a9ea55d2de251cbab131bb411de074ef1ed463a1
Tyler Gary <tygary@gmail.com>
2023-09-28 17:04:37 -0700

    Switch to arrow

--- timemachine/TimeMachine.py
+++ timemachine/TimeMachine.py
@@ -1,12 +1,10 @@
 from timemachine.Levers import *
-from datetime import datetime
-import time
+import arrow
 import math
 
 
-ZERO = datetime.fromtimestamp(0)
-END = datetime(2999, 12, 31, 23, 59, 59)
-START = datetime(1000, 1, 1, 0, 0, 0)
+END = arrow.get('2999-12-31 23:59:59', 'YYYY-MM-DD HH:mm:ss')
+START = arrow.get('1000-1-1 00:00:00', 'YYYY-MM-DD HH:mm:ss')
 # 10 years every second at full speed
 SPEED_MULTIPLIER = -100/1000 * 60 * 60 * 24 * 365
 ZERO_TOLERANCE = 0.1
@@ -14,7 +12,7 @@ MIN_UPDATE_TIME = 0.250
 
 
 def print_datetime(date):
-    return date.strftime('%a, %d %b %Y %H:%M:%S GMT')
+    return date.ctime()
 
 
 class TimeMachine(object):
@@ -22,7 +20,7 @@ class TimeMachine(object):
     date = END
     speed = 0
     active = True
-    last_event = time.time()
+    last_event = arrow.now()
 
     def __init__(self):
         self.levers = Levers(self.__on_lever_change, self.__on_button_change)
@@ -47,13 +45,13 @@ class TimeMachine(object):
     def update(self):
         self.levers.update()
         if self.active:
-            now = time.time()
+            now = arrow.now()
             time_delta = now - self.last_event
             if time_delta > MIN_UPDATE_TIME:
                 change = self.speed * SPEED_MULTIPLIER * time_delta
-                date_ts = (self.date - ZERO).total_seconds()
+                date_ts = self.date.float_timestamp
                 new_date_ts = round(date_ts + change)
-                new_date = datetime.fromtimestamp(new_date_ts)
+                new_date = arrow.get(new_date_ts)
                 if new_date > END:
                     new_date = END
                 if new_date < START:

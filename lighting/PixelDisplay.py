0ecd22cfbc08a8897774d3d7b19559996afcc772
Tyler Gary <tygary@gmail.com>
2023-10-06 16:05:57 -0700

    getting display working

--- lighting/PixelDisplay.py
+++ lighting/PixelDisplay.py
@@ -5,6 +5,7 @@ class PixelDisplay(object):
     matrix = Matrix()
 
     def draw_text(self, text):
+        self.matrix.reset()
         self.matrix.text(text, (0, 0), 8, (255, 0, 0))
         self.matrix.show()
 

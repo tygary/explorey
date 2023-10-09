376d30ab8f4c9826703dce0d0baf6fb439cf3620
Tyler Gary <tygary@gmail.com>
2023-10-09 16:09:10 -0700

    serial

--- timemachine/TimeDisplay.py
+++ timemachine/TimeDisplay.py
@@ -11,6 +11,7 @@ class TimeDisplay(object):
     display = PixelDisplay()
     mqtt = MqttClient()
     serial = UsbSerial("/dev/ttyACM0")
+    last_magnitude = 0
 
     def __init__(self):
         self.mqtt.listen(self.__on_event)
@@ -21,6 +22,9 @@ class TimeDisplay(object):
             data = json.loads(event)
             if data and data["date"]:
                 self.display.draw_text(data["date"])
-                output = numpy.int16(data['magnitude']).tobytes() + numpy.uint8(0).tobytes()
-                print(output)
-                self.serial.write(output)
+                magnitude = data["magnitude"]
+                if self.last_magnitude != magnitude:
+                    output = numpy.int16(magnitude).tobytes() + numpy.uint8(0).tobytes()
+                    print(output)
+                    self.serial.write(output)
+                    self.last_magnitude = magnitude

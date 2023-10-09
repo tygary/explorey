519bf221f8dd547f2d0b95b072a9bf60fe46a54d
Tyler Gary <tygary@gmail.com>
2023-10-09 15:02:46 -0700

    serial

--- lighting/UsbSerial.py
+++ lighting/UsbSerial.py
@@ -5,13 +5,14 @@ class UsbSerial(object):
     serial = None
 
     def __init__(self, port="/dev/ttyACM0"):
-        self.serial = serial.Serial(port, 9600)
+        self.serial = serial.Serial(port, 9600, timeout=5)
         # self.serial.open()
 
     def read(self):
         return self.serial.readLine()
 
     def write(self, message):
+        self.serial.flushOutput()
         return self.serial.write(message)
 
     def close(self):

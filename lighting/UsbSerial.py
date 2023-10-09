451dfaf426268eb9d909afcbefee25bb581a1729
Tyler Gary <tygary@gmail.com>
2023-10-09 16:17:16 -0700

    serial

--- lighting/UsbSerial.py
+++ lighting/UsbSerial.py
@@ -7,34 +7,30 @@ class UsbSerial(object):
 
     def __init__(self, port="/dev/ttyACM0"):
         self.port = port
-        try:
-            self.serial = serial.Serial(port, 9600, timeout=5)
-            self.is_connected = True
-        except:
-            self.serial = None
-            self.is_connected = False
-
+        self.__check_connection()
         # self.serial.open()
 
     def read(self):
+        self.__check_connection()
         if self.is_connected:
             return self.serial.readLine()
 
     def __check_connection(self):
-        try:
-            self.serial.inWaiting()
-        except:
-            print("Lost connection!")
-            self.is_connected = False
-
-    def write(self, message):
-        if not self.is_connected:
+        if self.is_connected:
+            try:
+                self.serial.inWaiting()
+            except:
+                print("Lost connection!")
+                self.is_connected = False
+        else:
             try:
                 self.serial = serial.Serial(self.port, 9600, timeout=5)
                 self.is_connected = True
             except:
                 self.is_connected = False
 
+    def write(self, message):
+        self.__check_connection()
         if self.is_connected:
             if self.serial.out_waiting > 100:
                 self.serial.reset_output_buffer()

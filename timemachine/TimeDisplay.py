66f1e63b5013e66128220e43b893cf9f4ece94b0
Tyler Gary <tygary@gmail.com>
2023-10-09 15:45:58 -0700

    serial

--- timemachine/TimeDisplay.py
+++ timemachine/TimeDisplay.py
@@ -21,6 +21,6 @@ class TimeDisplay(object):
             data = json.loads(event)
             if data and data["date"]:
                 self.display.draw_text(data["date"])
-                output = f"{str(data['magnitude']).zfill(4)}{0x0}"
+                output = f"{numpy.int16(data['magnitude']).tobytes()}{0x0}"
                 print(output)
                 self.serial.write(bytes(output, 'utf-8'))

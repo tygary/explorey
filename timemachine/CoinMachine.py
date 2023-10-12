b36e926dda9a11f7c170198dc53182f52c360a72
Tyler Gary <tygary@gmail.com>
2023-10-12 16:00:43 -0700

    coin machine

--- timemachine/CoinMachine.py
+++ timemachine/CoinMachine.py
@@ -9,8 +9,8 @@ from logger.logger import Logger
 # Coin Machine
 # -----------------------------------------------------------------------
 class CoinMachine(object):
-    coin_input_pin = board.D27
-    coin_counter_input_pin = board.D18
+    coin_input_pin = 27
+    coin_counter_input_pin = 18
     #coin_counter_pins = [29, 31]
 
     waiting_for_coin = False

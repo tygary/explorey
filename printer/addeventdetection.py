13c725e1cc5b7c6ff0341d796e4f494213456d63
Tyler Gary <tygary@gmail.com>
2023-10-12 17:44:28 -0700

    coin machine

--- printer/addeventdetection.py
+++ printer/addeventdetection.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 
 def add_event_detection(pin, callback, bothdirections=False, pullup=False):
     try:
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP if pullup else GPIO.PUD_DOWN)
         GPIO.remove_event_detect(pin)
         GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)

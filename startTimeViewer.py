32f24b517c30c86d105b0ed64b4d901afc00ce5c
Tyler Gary <tygary@gmail.com>
2023-10-16 17:18:24 -0700

    debugging

---
+++ startTimeViewer.py
@@ -0,0 +1,11 @@
+from timemachine.TimeViewer import TimeViewer
+import time
+
+print("Machine started, enter 'machine.stop()' before exiting")
+
+
+display = TimeViewer()
+
+while True:
+    time.sleep(0.05)
+    continue

b3b88a70a16c256c9ad85c5eb169d871d1b3ce2f
Tyler Gary <tygary@gmail.com>
2023-11-06 17:58:26 -0800

    DMX light debugging

--- lighting/Routine.py
+++ lighting/Routine.py
@@ -166,7 +166,7 @@ class ModeSwitchRoutine(TimeRoutine):
     def __init__(self, pixels, addresses):
         TimeRoutine.__init__(self, pixels, addresses)
         self.routines = [
-            PulseRoutine(pixels, [addresses[0]], color=[255, 255, 255])
+            PulseRoutine(pixels, [addresses[0]], color=[255, 255, 255]),
             BleuRoutine(pixels, [addresses[1]]),
             RainbowRoutine(pixels, [addresses[2]]),
         ]

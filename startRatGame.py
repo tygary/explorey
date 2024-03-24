66bac2c4f669929af9be0dd58b4ccc22582097b8
Tyler Gary <tygary@gmail.com>
2024-03-23 22:17:59 -0700

    updating

--- startRatGame.py
+++ startRatGame.py
@@ -6,10 +6,10 @@ print("Rat Game started, enter 'machine.stop()' before exiting")
 rat_game = RatGame()
 
 while True:
-    try:
-        rat_game.update()
-    except Exception as err:
-        print(err)
+    #try:
+    rat_game.update()
+    #except Exception as err:
+    #    print(err)
 
     time.sleep(0.05)
     continue

2bc11ca07e0842252937bd5053a46974f01047d7
Tyler Gary <tygary@gmail.com>
2024-05-26 21:05:57 -0700

    Testing

--- ratlantis/RatVineController.py
+++ ratlantis/RatVineController.py
@@ -20,26 +20,34 @@ class RatVineController(object):
 
     def __init__(self, controller_num=1):
         GPIO.setmode(GPIO.BCM)
-        self.pixels = PixelControl(400, led_brightness=180, led_pin=21)
+        self.pixels = PixelControl(700, led_brightness=180, led_pin=21)
         self.mqtt = MqttClient()
 
         if controller_num == 1:
             vine1 = EnergyVine(VINE_ONE_RFID, range(0, 174), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_BLUE)
             self.vines.append(vine1)
             vine2 = EnergyVine(VINE_TWO_RFID, range(174, 349), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_ORANGE)
             self.vines.append(vine2)
             vine3 = EnergyVine(VINE_THREE_RFID, range(349, 523), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_PINK)
             self.vines.append(vine3)
             vine4 = EnergyVine(VINE_FOUR_RFID, range(523, 697), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_GREEN)
             self.vines.append(vine4)
         elif controller_num == 2:
             vine1 = EnergyVine(VINE_FIVE_RFID, range(0, 174), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_YELLOW)
             self.vines.append(vine1)
             vine2 = EnergyVine(VINE_SIX_RFID, range(174, 349), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_PURPLE)
             self.vines.append(vine2)
             vine3 = EnergyVine(VINE_SEVEN_RFID, range(349, 523), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_LIGHT_BLUE)
             self.vines.append(vine3)
             vine4 = EnergyVine(VINE_EIGHT_RFID, range(523, 697), self.pixels, self.mqtt)
+            vine1.pending_connection(COLOR_RED)
             self.vines.append(vine4)
 
     def update(self):

c6f13c07612931286882a4f1db7153094d6466b1
Tyler Gary <tygary@gmail.com>
2024-05-26 21:44:09 -0700

    Testing

--- ratlantis/RatGame.py
+++ ratlantis/RatGame.py
@@ -53,7 +53,7 @@ class RatGame(object):
         self.artifacts.append(self.tank)
         self.tank.start_charging()
 
-        self.switchboard = Switchboard(self.pixels, [2, 3, 4, 5, 9, 10, 11, 12])
+        self.switchboard = Switchboard(self.pixels, [2, 3, 4, 5, 10, 11, 12, 13])
 
         self.game = GameLogic(self.vines, self.artifacts, self.tank, None)
         self.game.start()

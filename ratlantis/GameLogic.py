1762d03d333f0adc4e8511bae0ad1ee3afbf890f
Tyler Gary <tygary@gmail.com>
2024-05-28 00:02:30 -0700

    debug game

--- ratlantis/GameLogic.py
+++ ratlantis/GameLogic.py
@@ -218,6 +218,7 @@ class GameLogic(object):
                 return
             if not self.is_charging:
                 print("Game Starting Charging")
+                self.is_charging = True
                 self.energy_tank.start_charging()
                 self._update_vine_colors()
             if self.energy_tank.energy_level == 100 and not self.is_ready_to_start:

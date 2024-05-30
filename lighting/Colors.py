f476f75a7e65fa27d5d06d28d5119b07232deeb4
Tyler Gary <tygary@gmail.com>
2024-05-29 23:14:09 -0700

    debug game

--- lighting/Colors.py
+++ lighting/Colors.py
@@ -5,8 +5,8 @@ class Colors(object):
     green = [0, 255, 0, 0]
     blue = [0, 0, 255, 0]
 
-    orange = [255, 184, 3]
-    yellow = [230, 255, 4]
+    orange = [255, 184, 3, 0]
+    yellow = [230, 255, 4, 0]
 
     bright_white = [100, 100, 100, 200]
     soft_white = [20, 20, 20, 100]
@@ -14,11 +14,11 @@ class Colors(object):
     mid_green = [150, 250, 0, 50]
     light_green = [0, 250, 0, 100]
 
-    soft_blue = [2, 221, 255]
-    mixed_blue = [0, 50, 255, 100]
+    soft_blue = [2, 221, 255, 0]
+    mixed_blue = [0, 50, 255, 100, 0]
 
-    purple = [132, 2, 255]
-    pink = [255, 3, 191]
+    purple = [132, 2, 255, 0]
+    pink = [255, 3, 191, 0]
 
     def get_random_color_weighted(self):
         all_colors = self.get_all_colors()

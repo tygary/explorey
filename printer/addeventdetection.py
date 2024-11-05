497b99cc1af9c733c2177df3c0969d857121e6f6
Tyler Gary <tyler.gary@benchling.com>
2024-11-04 16:10:06 -0800

    Audio machine

--- printer/addeventdetection.py
+++ printer/addeventdetection.py
@@ -4,17 +4,20 @@ import RPi.GPIO as GPIO
 def add_event_detection(pin, callback, bothdirections=False, pullup=False):
     try:
         pull_up_down = GPIO.PUD_UP if pullup else GPIO.PUD_DOWN
+        if bothdirections:
+            edge = GPIO.BOTH
+        elif pullup:
+            edge = GPIO.FALLING
+        else:
+            edge = GPIO.RISING
+        edge = GPIO.BOTH if bothdirections else GPIO.RISING
         GPIO.setup(pin, GPIO.IN, pull_up_down=pull_up_down)
         GPIO.remove_event_detect(pin)
-        GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
-        if bothdirections:
-            GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+        GPIO.add_event_detect(pin, edge, callback=callback)
     except RuntimeError:
         # try:
         GPIO.remove_event_detect(pin)
-        GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
-        if bothdirections:
-            GPIO.add_event_detect(pin, GPIO.FALLING, callback=callback)
+        GPIO.add_event_detect(pin, edge, callback=callback)
         # except RuntimeError as e:
         #     pass
         #     print(f"Error during Event Detection: {e}")

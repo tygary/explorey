e164f53960ac3b21ba3a8c101735edb89b8fa195
Tyler Gary <tyler.gary@benchling.com>
2024-11-07 21:28:55 -0800

    Printer

---
+++ startGhostScale.py
@@ -0,0 +1,15 @@
+from ghost.ScaleMachine import GhostScaleMachine
+import time
+
+print("Ghost scale started, enter 'machine.stop()' before exiting")
+
+machine = GhostScaleMachine()
+
+while True:
+    #try:
+    machine.update()
+    #except Exception as err:
+    #    print(err)
+
+    time.sleep(0.05)
+    continue

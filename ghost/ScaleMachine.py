28a4a1408b7a666a016e242346bc5ea8467dfafd
Tyler Gary <tyler.gary@benchling.com>
2024-11-11 19:31:37 -0800

    Printer

--- ghost/ScaleMachine.py
+++ ghost/ScaleMachine.py
@@ -66,7 +66,7 @@ class GhostScaleMachine(object):
     current_balance = 50
     last_game_balance_update = 0
 
-    oscillation_period_s = 1
+    oscillation_period_ms = 1000
     oscillation_start_time = 0
     oscillation_going_up = True
     oscillation_magnitude = 2
@@ -325,13 +325,13 @@ class GhostScaleMachine(object):
             return
         now = time.time() * 1000
         if self.oscillation_start_time < now:
-            self.oscillation_start_time = now + self.oscillation_period_s
+            self.oscillation_start_time = now + self.oscillation_period_ms
             self.oscillation_going_up = not self.oscillation_going_up
             print("oscillation flipping", self.oscillation_going_up)
         if self.oscillation_going_up:
-            self.oscillated_balance = self.current_balance + round(math.sin(math.pi * (now - self.oscillation_start_time) / self.oscillation_period_s) * self.oscillation_magnitude)
+            self.oscillated_balance = self.current_balance + round(math.sin(math.pi * (now - self.oscillation_start_time) / self.oscillation_period_ms) * self.oscillation_magnitude)
         else:
-            self.oscillated_balance = self.current_balance - math.sin(math.pi * (now - self.oscillation_start_time) / self.oscillation_period_s) * self.oscillation_magnitude
+            self.oscillated_balance = self.current_balance - math.sin(math.pi * (now - self.oscillation_start_time) / self.oscillation_period_ms) * self.oscillation_magnitude
 
     def start_end_game(self):
         print("Game Over")

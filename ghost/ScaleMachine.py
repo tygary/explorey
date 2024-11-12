fee09ec338aecaeab15fd5a8c9ecd9afb62a5e11
Tyler Gary <tyler.gary@benchling.com>
2024-11-11 17:16:48 -0800

    Printer

--- ghost/ScaleMachine.py
+++ ghost/ScaleMachine.py
@@ -145,6 +145,7 @@ class GhostScaleMachine(object):
                     Routines.BlackoutRoutine(self.pixels, left),
                     Routines.RainbowRoutine(self.pixels, right),
                 ]
+        self.previous_mode = self.mode
 
     def button_one_pressed(self):
         print("Button one pressed")

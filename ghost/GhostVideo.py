02855fb5ac09756cf55289947f2c5c6e6361b992
Tyler Gary <tyler.gary@benchling.com>
2024-11-13 00:33:28 -0800

    Scale

--- ghost/GhostVideo.py
+++ ghost/GhostVideo.py
@@ -1,11 +1,13 @@
-import vlc
+# import vlc
 
 
 class GhostVideo(object):
     def __init__(self):
-        self.player = vlc.MediaPlayer("/home/admin/explorey/ghost/ghost_pepper.mp4")
-        self.player.play()
+        # self.player = vlc.MediaPlayer("/home/admin/explorey/ghost/ghost_pepper.mp4")
+        # self.player.play()
+        pass
 
     def play(self):
-        self.player.stop()
-        self.player.play()
+        # self.player.stop()
+        # self.player.play()
+        pass

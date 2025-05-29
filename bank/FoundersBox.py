ce03404bfe8de03f7e2a8a1e30045231cb40fa4d
Tyler Gary <tygary@gmail.com>
2025-05-28 23:00:45 -0700

    Founders box changes

--- bank/FoundersBox.py
+++ bank/FoundersBox.py
@@ -7,6 +7,7 @@ from lighting.PixelControl import OverlayedPixelControl
 from lighting.Colors import Colors, get_grb_color
 from lighting.routines import Routines
 from mqtt.MqttClient import MqttClient
+from bank.FoundersBoxSoundSystem import FoundersBoxSoundSystem
 
 from timemachine.Levers import *
 # from timemachine.Button import Button
@@ -25,7 +26,7 @@ BOTTOM_PIXELS = range(0, 50)
 
 UNLOCK_TIMEOUT_S = 10
 
-LOCK_PIN = 17
+LOCK_PIN = 20
 
 class FoundersBox(object):
     id = "founders_box"
@@ -39,22 +40,26 @@ class FoundersBox(object):
         self.mqtt = MqttClient()
         self.levers = Levers(self.__on_lever_change, self.__on_button_change)
         self.mqtt.listen(self.__parse_mqtt_event)
-        self.pixels = OverlayedPixelControl(led_count=LED_COUNT, led_brightness=255)
-        GPIO.setup(LOCK_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_UP)
+        self.sound = FoundersBoxSoundSystem()
+        self.pixels = OverlayedPixelControl(led_count=LED_COUNT, led_brightness=70, led_pin=21)
+        GPIO.setup(LOCK_PIN, GPIO.OUT)
         self._update_light_routines()
         self.reset()
 
     def __on_lever_change(self, lever_id, value):
+        if lever_id != 1:
+            return
         raw_lever_value = self.raw_lever_values[lever_id]
         if raw_lever_value != value:
             # print(f"Got lever {id} change to {value}")
             modified_value = value * 100 * -1
             modified_prev_value = raw_lever_value * 100 * -1
-            if abs(modified_value - modified_prev_value) > 1:
+            if abs(modified_value - modified_prev_value) > 2:
                 rounded_value = round(modified_value)
                 self.raw_lever_values[lever_id] = value
                 self.magnitudes[lever_id] = rounded_value * 10
                 print(f"magnitudes: {self.magnitudes}")
+                self.sound.play_lever()
                 self.__on_change_data()
 
     def __on_change_data(self):
@@ -95,15 +100,16 @@ class FoundersBox(object):
         print("Unlocking")
         self.lock_time = time.time() + UNLOCK_TIMEOUT_S
         self.mode = MODE_OPEN
-        GPIO.output(LOCK_PIN, GPIO.LOW)
+        GPIO.output(LOCK_PIN, GPIO.HIGH)
         self._update_light_routines()
 
     def lock(self):
         print("Locking")
         self.lock_time = 0
         self.mode = MODE_CLOSED
-        GPIO.output(LOCK_PIN, GPIO.HIGH)
+        GPIO.output(LOCK_PIN, GPIO.LOW)
         self._update_light_routines()
+        self.sound.play_open()
 
     def reset(self):
         print("Resetting")
@@ -111,7 +117,9 @@ class FoundersBox(object):
         self.raw_lever_values = [0, 0, 0]
         self.magnitudes = [0, 0, 0]
         self.lock_time = 0
+        self.lock()
         self._update_light_routines()
+        self.sound.play_background()
 
     def update(self):
         self.levers.update()
---
+++ bank/FoundersBoxSoundSystem.py
@@ -0,0 +1,33 @@
+from sound.MultiTrackMusicPlayer import MultiTrackMusicPlayer
+import random
+
+OFF = -1
+BACKGROUND = 0
+LEVER = 1
+OPEN = 2
+
+CHANNEL_MUSIC = 1
+CHANNEL_LEVER = 2
+CHANNEL_OPEN = 3
+
+songs = [
+    '/home/admin/explorey/sound/Scale-background.ogg',
+    '/home/admin/explorey/sound/Scale-hit-0.ogg',
+    '/home/admin/explorey/sound/Scale-ready.ogg',
+]
+
+
+class FoundersBoxSoundSystem(object):
+    player = MultiTrackMusicPlayer(songs, devicename='bcm2835 Headphones, bcm2835 Headphones')
+
+    def play_lever(self):
+        if not self.player.is_still_playing(CHANNEL_LEVER):
+            self.player.play_song(LEVER, 0.6, channel_num=CHANNEL_LEVER, loops=0)
+
+    def play_open(self):
+        self.player.stop_music(CHANNEL_OPEN)
+        self.player.play_song(OPEN, 0.4, channel_num=CHANNEL_OPEN, loops=0)
+
+    def play_background(self):
+        self.player.stop_music(CHANNEL_MUSIC)
+        self.player.play_song(BACKGROUND, 0.4, channel_num=CHANNEL_MUSIC, loops=-1)

42bd8325b4d0d8885b46b3732e553aac33022fa8
Tyler Gary <tylergary@Tylers-MacBook-Pro-2.local>
2025-05-30 21:08:48 -0700

    teller

--- bank/ui/UI.py
+++ bank/ui/UI.py
@@ -202,7 +202,7 @@ class UiApp(App):
         if not account:
             show_toast(self.dashboard, "Failed to deposit into account.  Account not found")
             return
-        show_toast(self.dashboard, f"Deposited {amount} beans with exchange rate {exchange_rate} for {beanbucks} BeanBucks into account: {form_info.to_account_number}. New Balance: {account.balance}")
+        show_toast(self.dashboard, f"Deposited {amount} beans with exchange rate {exchange_rate}\nfor {beanbucks} BeanBucks into account: {form_info.to_account_number}.\nNew Balance: {account.balance}")
 
     def on_finish_scanning_withdrawl(self, form_info: FormInfo):
         account, amount, beanbucks, exchange_rate = self.atm.withdraw(form_info.from_account_number)
@@ -211,7 +211,7 @@ class UiApp(App):
             show_toast(self.dashboard, "Failed to withdraw from account.  Account not found")
             return
         self.dispense_beans(amount)
-        show_toast(self.dashboard, f"Withdrew {beanbucks} BeanBucks with exchange rate {exchange_rate} from account: {form_info.from_account_number}.  Dispensing {amount} Beans.  New Balance: {account.balance}")
+        show_toast(self.dashboard, f"Withdrew {beanbucks} BeanBucks with exchange rate {exchange_rate} from account: {form_info.from_account_number}.\nDispensing {amount} Beans.\nNew Balance: {account.balance}")
 
     def on_finish_scanning_transfer(self, form_info: FormInfo):
         def on_amount_confirmation(amount):

025eb1fe72b2e6376b24ff829e0d8b910207fc88
Tyler Gary <tylergary@Tylers-MacBook-Pro-2.local>
2025-05-30 18:10:15 -0700

    teller

--- bank/ui/BankruptcyConfirmationScreen.py
+++ bank/ui/BankruptcyConfirmationScreen.py
@@ -3,6 +3,7 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
+from kivy.uix.widget import Widget
 
 class BankruptcyConfirmationScreen(Screen):
     def __init__(self, on_finalize_bankruptcy, **kwargs):
@@ -12,13 +13,12 @@ class BankruptcyConfirmationScreen(Screen):
 
         # Left side layout
         self.left_layout = BoxLayout(orientation='vertical', size_hint=(0.7, 1), padding=10, spacing=10)
-        self.account_image = Image(size_hint=(1, None), height=200, allow_stretch=True, keep_ratio=True)
+        self.account_image = Image(size_hint=(1, None), width=200, allow_stretch=True, keep_ratio=True)
         self.beans_owed_label = Label(font_size="18sp", size_hint=(1, None), height=50)
         self.instructions_label = Label(
             text="It has come to our attention that you have decided to file for bankruptcy.   While it is unfortunate you have been unable to repay your debts, ACME Bank is very understanding that sometimes people are very bad with money and this does not make them bad people.\n\nIn order to qualify for bankruptcy, you must have completed the bankruptcy form.\n\nDo you swear that you have done one or more of the following:\n\n  * Attempted to work off your debt as a teller\n  * Announced loudly to everyone in the bank that you are bad with beans and apologize\n  * Visited the spank bank and received your corporate punishment by a teller\n\nIf so, then you may file for bankruptcy.  \n\nBy filing for bankruptcy, do you agree to the following:\n\n  * Your debt will be wiped away and replaced by a single bean in your account\n  * You vow to be better with money and not go into debt\n  * You understand that by going into debt a second time that ACME Bank reserves the right to foreclose on any belongings, property, clothes, nicknames, beers in your cooler, or artistic credits you may own.\n\nPress the button below to continue.",
             font_size="16sp",
             size_hint=(1, None),
-            height=300,
             width=400,
             text_size=(400, None),
             halign="left",
@@ -32,6 +32,7 @@ class BankruptcyConfirmationScreen(Screen):
         self.left_layout.add_widget(self.account_image)
         self.left_layout.add_widget(self.beans_owed_label)
         self.left_layout.add_widget(self.instructions_label)
+        self.left_layout.add_widget(Widget(size_hint_y=0.2))
         self.left_layout.add_widget(agree_button)
 
         # Right side layout
